2a371fdf33a933ad826a2a5fbe5b9ae5db262271
TatevikDemirchyan <tatevikdemirchyan@gmail.com>
2019-08-18 13:06:47 +0400

    add new file

---
+++ Lecture1/Practical/Problem3.py
@@ -0,0 +1,10 @@
+course= "Intro to Python"
+student="Tatevik Demirchyan"
+nickname="Tatev"
+grade=100
+python_expert=True
+print("course:",course)
+print("student:",student)                     
+print("nickname:",nickname)                   
+print("grade:",grade)                   
+print("python_expert:",python_expert)
