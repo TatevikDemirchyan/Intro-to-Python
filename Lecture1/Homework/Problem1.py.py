d1521cfcc53eacb72e0ceacc5f912f23ab6c82df
TatevikDemirchyan <tatevikdemirchyan@gmail.com>
2019-08-24 20:37:33 +0400

    2

---
+++ Lecture1/Homework/Problem1.py.py
@@ -0,0 +1,27 @@
+project="cake"
+difficulty="4"
+ingredients=["flour","butter", "sugar", "eggs", "cocoa powder", "baking powder"]
+print("apples" in ingredients,"\n",
+      "butter" in ingredients, "\n",
+      'eggs' in ingredients or "margarine" in ingredients,"\n",
+      'eggs' in ingredients and "margarine" in ingredients)
+
+flour=175
+butter=175
+sugar="100g"
+eggs=2
+cocoa_powder="1ts"
+baking_powder=0.5
+print (type(flour), "\n",
+type(butter),"\n",
+type(sugar),"\n",
+type(eggs),"\n",
+type(cocoa_powder),"\n",
+type(baking_powder))
+
+print ("flour=",flour,"\n",
+       "butter=",butter,"\n",
+       "sugar=",sugar,"\n",
+       "eggs=",eggs,"\n",
+       "cocoa_powder=",cocoa_powder,"\n",
+       "baking_powder=",baking_powder)
---
+++ Lecture1/Homework/Problem4.py.py
@@ -0,0 +1,5 @@
+number=int(input("Մուտքագրեք թիվ"))
+if number%2==0:
+    print(number,"- even")
+else:
+    print(number,"- odd")
