ddce37aa013abe255cba1e33ed303e90b980bff0
TatevikDemirchyan <tatevikdemirchyan@gmail.com>
2019-09-15 22:32:22 +0400

    15.09.19

---
+++ Lecture5/Practical/calc.py
@@ -0,0 +1,13 @@
+import pretty_print
+def calculate_squere(x):
+    return x**2
+
+def calculate_cube(x):
+    return x**3
+
+def main():
+    pretty_print.simple_print(calculate_square(2))
+    pretty_print.pro_print(calculate_cube(4))
+
+if __name__ == '__main__':
+    main()
